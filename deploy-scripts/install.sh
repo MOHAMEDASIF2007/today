a973218807d55e86de4fb0c23c7df0e48da15e48
MOHAMEDASIF I <mohamedasif.i2024it@sece.ac.in>
2026-04-08 11:48:57 +0530

    Create install.sh

---
+++ deploy-scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt update -y
+apt install nginx -y
+
+systemctl enable nginx
+systemctl start nginx
+
+rm -f /var/www/html/index.html
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
